e4fdaafcf60d1d1a936a7ae95f8219b78c41f1b8
HigorMatheusip <147100075+HigorMatheusip@users.noreply.github.com>
2024-11-18 01:24:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Professora',
+    'Qual a melhor professora?',
+    'A melhor professora é a Rosângela'
+)
+
+criaCartao(
+    'Terceirão',
+    'Qual o melhor aluno do terceirão?',
+    'O melhora aluno do terceirâo é o Higor Matheus'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual a maior seleção do mundo?',
+    'A maior seleção do mundo é a seleção brasileira'
+)
